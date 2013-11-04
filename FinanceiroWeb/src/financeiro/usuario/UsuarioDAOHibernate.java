2a666c5e3fe8b9649e789bfbef748fed6f5f12fa
jeanroldao <jeanroldao@gmail.com>
2013-11-04 20:32:07 -0200

    permissoes de usuario e fix ao salvar as permissões
    Signed-off-by: jeanroldao <jeanroldao@gmail.com>

--- FinanceiroWeb/src/financeiro/usuario/UsuarioDAOHibernate.java
+++ FinanceiroWeb/src/financeiro/usuario/UsuarioDAOHibernate.java
@@ -18,5 +18,15 @@ public class UsuarioDAOHibernate extends GenericDAOHibernate<Usuario> implements
 		consulta.setString(":login", login);
 		return (Usuario) consulta.uniqueResult();
 	}
+	
+	@Override
+	public void atualizar(Usuario usuario) {
+		if (usuario.getPermissao() == null || usuario.getPermissao().size() == 0) {
+			Usuario usuarioPermissao = this.carregar(usuario.getCodigo());
+			usuario.setPermissao(usuarioPermissao.getPermissao());
+			session.evict(usuarioPermissao);
+		}
+		super.atualizar(usuario);
+	}
 
 }

eeaa5d9eea5da0cc094ec8115ff7f6b408ce4feb
jeanroldao <jeanroldao@gmail.com>
2013-11-06 20:57:30 -0200

    sql tabela conta bancaria
    Signed-off-by: jeanroldao <jeanroldao@gmail.com>

--- FinanceiroWeb/sql/financeiro.sql
+++ FinanceiroWeb/sql/financeiro.sql
@@ -20,3 +20,18 @@ create table usuario_permissao (
 	usuario int,
 	permissao varchar(255)
 );
+
+create table conta_bancaria (
+	cod_conta int not null auto_increment,
+	cod_usuario int not null,
+	des_conta varchar(255) default null,
+	dat_cadastro datetime not null,
+	saldo_inicial float default null,
+	favorita char(1) default null,
+	primary key (cod_conta),
+	key fk_conta_usuario (cod_usuario),
+	constraint fk_conta_usuario
+		foreign key (cod_usuario)
+		references usuairo (codigo)
+		on delete cascade
+);

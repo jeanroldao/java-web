4cff9ac9b381105ff3e5202fb210ba71a15abdd4
jeanroldao <jeanroldao@gmail.com>
2014-03-01 21:30:52 -0300

    LancamentoBean p363
    Signed-off-by: jeanroldao <jeanroldao@gmail.com>

---
+++ FinanceiroWeb/src/financeiro/web/LancamentoBean.java
@@ -0,0 +1,89 @@
+package financeiro.web;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import financeiro.categoria.Categoria;
+import financeiro.conta.Conta;
+import financeiro.lancamento.Lancamento;
+import financeiro.lancamento.LancamentoBO;
+import financeiro.web.util.ContextoUtil;
+
+@ManagedBean(name = "lancamentoBean")
+@ViewScoped
+public class LancamentoBean {
+	
+	private List<Lancamento> lista;
+	private List<Double> saldos = new ArrayList<Double>();;
+	private float saldoGeral;
+	
+	private Lancamento editado;
+	
+	//@ManagedProperty("contextoBean")
+	//private ContextoBean contextoBean_test;
+	
+	public LancamentoBean() {
+		novo();
+	}
+	
+	public void novo() {
+		editado = new Lancamento();
+		editado.setData(new Date(System.currentTimeMillis()));
+	}
+	
+	public void editar() {
+		
+	}
+	
+	public void salvar() {
+		ContextoBean contextoBean = ContextoUtil.getContextoBean();
+		editado.setUsuario(contextoBean.getUsuarioLogado());
+		editado.setConta(contextoBean.getContaAtiva());
+		
+		LancamentoBO lancamentoBO = new LancamentoBO();
+		lancamentoBO.salvar(editado);
+		
+		novo();
+		lista = null;
+	}
+	
+	public void excluir() {
+		LancamentoBO lancamentoBO = new LancamentoBO();
+		editado = lancamentoBO.carregar(editado.getLancamento());
+		lancamentoBO.excluir(editado);
+		lista = null;
+	}
+	
+	public List<Lancamento> getLista() {
+		if (lista == null) {
+			ContextoBean contextoBean = ContextoUtil.getContextoBean();
+			Conta conta = contextoBean.getContaAtiva();
+			
+			Calendar dataSaldo = new GregorianCalendar();
+			dataSaldo.add(Calendar.MONTH, -1);
+			dataSaldo.add(Calendar.DAY_OF_MONTH, -1);
+			
+			Calendar inicio = new GregorianCalendar();
+			inicio.add(Calendar.MONTH, -1);
+			
+			LancamentoBO lancamentoBO = new LancamentoBO();
+			saldoGeral = lancamentoBO.saldo(conta, dataSaldo.getTime());
+			lista = lancamentoBO.listar(conta, inicio.getTime(), null);
+			
+			Categoria categoria = null;
+			double saldo = saldoGeral;
+			for (Lancamento lancamento : lista) {
+				categoria = lancamento.getCategoria();
+				saldo += lancamento.getValor().floatValue() * categoria.getFator();
+				saldos.add(saldo);
+			}
+		}
+		return lista;
+	}
+}

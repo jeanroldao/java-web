94134c23de1ec2a41b7ad2f9e517eaab292b0bdf
jeanroldao <jeanroldao@gmail.com>
2014-03-06 23:27:35 -0300

    BOException message constructor p382
    Signed-off-by: jeanroldao <jeanroldao@gmail.com>

--- FinanceiroWeb/src/financeiro/util/BOException.java
+++ FinanceiroWeb/src/financeiro/util/BOException.java
@@ -3,5 +3,8 @@ package financeiro.util;
 public class BOException extends Exception {
 
 	private static final long serialVersionUID = -5410904744413059150L;
-
+	
+	public BOException(String msg) {
+		super(msg);
+	}
 }

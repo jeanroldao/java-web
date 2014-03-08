eebc0e6875e4d2e3894059547d96e4f0305c4c6b
jeanroldao <jeanroldao@gmail.com>
2014-03-07 23:18:56 -0300

    ChequeBean stub p385
    Signed-off-by: jeanroldao <jeanroldao@gmail.com>

---
+++ FinanceiroWeb/src/financeiro/web/ChequeBean.java
@@ -0,0 +1,10 @@
+package financeiro.web;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "chequeBean")
+@RequestScoped
+class ChequeBean {
+	
+}

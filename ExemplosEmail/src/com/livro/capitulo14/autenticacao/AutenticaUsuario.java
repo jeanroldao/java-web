ce3a9029df2046d07935106fe0e988ad09091a2a
jeanroldao <jeanroldao@gmail.com>
2014-03-30 21:47:49 -0300

    AutenticaUsuario p466
    Signed-off-by: jeanroldao <jeanroldao@gmail.com>

---
+++ ExemplosEmail/src/com/livro/capitulo14/autenticacao/AutenticaUsuario.java
@@ -0,0 +1,18 @@
+package com.livro.capitulo14.autenticacao;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class AutenticaUsuario extends Authenticator {
+	private String usuario;
+	private String senha;
+	
+	public AutenticaUsuario(String usuario, String senha) {
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+	
+	public PasswordAuthentication getPasswordAuthentication() {
+		return new PasswordAuthentication(usuario, senha);
+	}
+}

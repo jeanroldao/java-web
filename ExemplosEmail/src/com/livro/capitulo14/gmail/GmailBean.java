ee350c7871abd8273b91e5fae17286bc79754a9b
jeanroldao <jeanroldao@gmail.com>
2014-04-13 23:31:31 -0300

    Removido enviarEmailAsync p481
    Signed-off-by: jeanroldao <jeanroldao@gmail.com>

--- ExemplosEmail/src/com/livro/capitulo14/gmail/GmailBean.java
+++ ExemplosEmail/src/com/livro/capitulo14/gmail/GmailBean.java
@@ -2,8 +2,6 @@ package com.livro.capitulo14.gmail;
 
 import java.util.Date;
 import java.util.Properties;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -18,7 +16,6 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import com.livro.capitulo14.autenticacao.AutenticaUsuario;
-import com.sun.mail.smtp.SMTPMessage;
 
 @ManagedBean(name = "gmailBean")
 @RequestScoped
@@ -27,78 +24,11 @@ public class GmailBean {
 	public static final String SERVIDOR_SMTP = "smtp.gmail.com";
 	public static final String PORTA_SERVIDOR_SMTP = "465";
 	
-	private static int contador = 0;
-	
-	private static Executor pool = Executors.newSingleThreadExecutor();
-	
 	private String usuario;
 	private String senha;
 	private String para;
 	private String assunto;
 	private String mensagem;
-	
-	public void enviarEmailAsync() {
-		final String usuario = this.usuario, 
-					 senha = this.senha,
-					 para = this.para,
-					 assunto = this.assunto,
-					 mensagem = this.mensagem;
-		pool.execute(new Runnable() {
-			public void run() {
-				//FacesContext context = FacesContext.getCurrentInstance();
-				
-				AutenticaUsuario autenticaUsuario = new AutenticaUsuario(usuario, senha);
-				Properties config = new Properties();
-				
-				config.setProperty("mail.transport.protocol", "smtp"); 
-				config.setProperty("mail.smtp.starttls.enable", "true");
-				config.setProperty("mail.smtp.host", SERVIDOR_SMTP); // Servidor SMTP do GMAIL
-				config.setProperty("mail.smtp.auth", "true"); // Ativa autenticação
-				config.setProperty("mail.smtp.user", usuario);
-				config.setProperty("mail.debug", "true");
-				config.setProperty("mail.smtp.port", PORTA_SERVIDOR_SMTP); // mesma porta para o socket
-				config.setProperty("mail.smtp.socketFactory.port", PORTA_SERVIDOR_SMTP);
-				config.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-				config.setProperty("mail.smtp.socketFactory.fallback", "false");
-				
-				Session session = Session.getDefaultInstance(config, autenticaUsuario);
-				
-				// Habilita o LOG das ações executadas durante o envio do email
-				session.setDebug(true);
-				
-				try {
-					Transport envio = session.getTransport("smtp");					
-					envio.connect(SERVIDOR_SMTP, usuario, senha);
-					
-					MimeMessage email = new MimeMessage(session);
-					email.setRecipient(Message.RecipientType.TO, new InternetAddress(para));
-					email.setFrom(new InternetAddress(usuario));
-					email.setSubject(assunto + " (" + (contador++) + ")");
-					email.setContent(mensagem, "text/plain");
-					email.setSentDate(new Date());
-					email.saveChanges();
-					
-					SMTPMessage smtpMessage = new SMTPMessage(email);
-					smtpMessage.setHeader("Return-Receipt-To", usuario);
-					smtpMessage.setHeader("Disposition-Notification-To", usuario);
-					smtpMessage.setReturnOption(SMTPMessage.RETURN_FULL);
-					smtpMessage.setNotifyOptions(SMTPMessage.NOTIFY_SUCCESS);
-					
-					envio.sendMessage(smtpMessage, email.getAllRecipients());
-					
-					envio.close();
-					
-					//context.addMessage(null, new FacesMessage("E-mail enviado com sucesso"));
-					
-				} catch (AddressException e) {
-					//context.addMessage(null, new FacesMessage("Erro ao montar mensagem de e-Mail! Erro:" + e.getMessage()));
-				} catch (MessagingException e) {
-					//context.addMessage(null, new FacesMessage("Erro ao enviar e-Mail! Erro:" + e.getMessage()));
-				}
-			}
-		});
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("E-mail enviado async"));
-	}
 	
 	public void enviarEmail() {
 		FacesContext context = FacesContext.getCurrentInstance();
@@ -113,7 +43,7 @@ public class GmailBean {
 			MimeMessage email = new MimeMessage(session);
 			email.setRecipient(Message.RecipientType.TO, new InternetAddress(this.para));
 			email.setFrom(new InternetAddress(this.usuario));
-			email.setSubject(this.assunto + " (" + (contador++) + ")");
+			email.setSubject(this.assunto);
 			email.setContent(this.mensagem, "text/plain");
 			email.setSentDate(new Date());
 			

c88e9d105bb5b6acae336cc5ce1d75ff7ae4cfba
jeanroldao <jeanroldao@gmail.com>
2014-04-25 20:44:03 -0300

    UsuarioBO metodo para enviar email pos cadastramento p486
    Signed-off-by: jeanroldao <jeanroldao@gmail.com>

--- FinanceiroWeb/src/financeiro/usuario/UsuarioBO.java
+++ FinanceiroWeb/src/financeiro/usuario/UsuarioBO.java
@@ -1,9 +1,14 @@
 package financeiro.usuario;
 
 import java.util.List;
+import java.util.Locale;
 
 import financeiro.categoria.CategoriaBO;
+import financeiro.util.BOException;
 import financeiro.util.DAOFactory;
+import financeiro.util.UtilException;
+import financeiro.web.util.EmailUtil;
+import financeiro.web.util.MensagemUtil;
 
 public class UsuarioBO {
 	
@@ -43,5 +48,21 @@ public class UsuarioBO {
 	
 	public List<Usuario> listar() {
 		return usuarioDAO.listar();
+	}
+	
+	public void enviarEmailPosCadastramento(Usuario usuario) throws BOException {
+		
+		// Enviando um email conforme o idioma do usuario
+		String[] info = usuario.getIdioma().split("_");
+		Locale locale = new Locale(info[0], info[1]);
+		String titulo = MensagemUtil.getMensagem(locale, "email_titulo");
+		String mensagem = MensagemUtil.getMensagem(locale, "email_mensagem", usuario.getNome(), usuario.getLogin(), usuario.getSenha());
+		
+		try {
+			EmailUtil emailUtil = new EmailUtil();
+			emailUtil.enviarEmail(null, usuario.getEmail(), titulo, mensagem);
+		} catch (UtilException e) {
+			throw new BOException(e);
+		}
 	}
 }

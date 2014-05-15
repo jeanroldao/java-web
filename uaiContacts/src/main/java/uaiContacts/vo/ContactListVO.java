cd72d9b3cc993168073c76ecdef425f75217a82b
jeanroldao <jeanroldao@gmail.com>
2014-05-14 22:07:07 -0300

    ContactListVO
    Signed-off-by: jeanroldao <jeanroldao@gmail.com>

--- uaiContacts/src/main/java/uaiContacts/vo/ContactListVO.java
+++ uaiContacts/src/main/java/uaiContacts/vo/ContactListVO.java
@@ -1,5 +1,56 @@
 package uaiContacts.vo;
 
-public class ContactListVO {
+import java.util.List;
 
+import uaiContacts.model.Contact;
+
+public class ContactListVO {
+	
+	private String actionMessage;
+	private String searchMessage;
+	
+	private int totalPages;
+	private long totalElements;
+	private List<Contact> content;
+
+	public ContactListVO() {
+		
+	}
+	
+	public ContactListVO(int totalPages, long totalElements,
+			List<Contact> content) {
+		this.totalPages = totalPages;
+		this.totalElements = totalElements;
+		this.content = content;
+	}
+	public String getActionMessage() {
+		return actionMessage;
+	}
+	public void setActionMessage(String actionMessage) {
+		this.actionMessage = actionMessage;
+	}
+	public String getSearchMessage() {
+		return searchMessage;
+	}
+	public void setSearchMessage(String searchMessage) {
+		this.searchMessage = searchMessage;
+	}
+	public int getTotalPages() {
+		return totalPages;
+	}
+	public void setTotalPages(int totalPages) {
+		this.totalPages = totalPages;
+	}
+	public long getTotalElements() {
+		return totalElements;
+	}
+	public void setTotalElements(long totalElements) {
+		this.totalElements = totalElements;
+	}
+	public List<Contact> getContent() {
+		return content;
+	}
+	public void setContent(List<Contact> content) {
+		this.content = content;
+	}
 }

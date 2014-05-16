72722590769ee2f775c2f1b3eafa85f679ea0636
jeanroldao <jeanroldao@gmail.com>
2014-05-16 20:49:45 -0300

    LoginController
    Signed-off-by: jeanroldao <jeanroldao@gmail.com>

---
+++ uaiContacts/src/main/java/uaiContacts/controller/LoginController.java
@@ -0,0 +1,20 @@
+package uaiContacts.controller;
+
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	@RequestMapping(method = {GET, POST, DELETE, PUT})
+	public ModelAndView doGet() {
+		return new ModelAndView("login");
+	}
+}
